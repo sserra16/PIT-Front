f76b15ef362947d92bfb6fc23e0ff0adc5f166df
SSerra007 <samuel.serra@aglets.com.br>
2023-11-09 12:20:18 -0300

    mudar api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:3333",
+  baseURL: "http://3.89.111.239:3333",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "*",
